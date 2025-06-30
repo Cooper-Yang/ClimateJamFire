9c97896db93de3f31c8b1f4cbdbdf382c284c26e
pjheric <pjheric1219@gmail.com>
2025-06-30 16:29:47 +0900

    phase change screen & ability tooltips, fonts

---
+++ Assets/Scripts/UI/TooltipHover.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro; 
+public class TooltipHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private GameObject tooltipObject;
+    [SerializeField] private Ability ability; 
+    [SerializeField] private TextMeshProUGUI abilityTitle;
+    [SerializeField] private TextMeshProUGUI abilityDescription;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tooltipObject.SetActive(true);
+        UpdateTooltip(); 
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tooltipObject.SetActive(false); 
+    }
+
+    public void UpdateTooltip()
+    {
+        abilityTitle.text = ability.abilityName; 
+        abilityDescription.text = ability.abilityDescription;
+    }
+}
